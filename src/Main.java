858aa2c2842ac5a5dc4d87a845011ded29961922
ImDani163 <68122758+ImDani163@users.noreply.github.com>
2024-06-15 11:58:39 +0200

    Clase 1

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String welcome = "Hola mundo desde Java";
+        System.out.println(welcome);
+
+        System.out.println(welcome.toUpperCase());
+
+        int numero = 10;
+        boolean valor = true;
+        int numero2 = 10;
+
+        if(valor){
+            System.out.println(numero);
+            numero2 = 15;
+        }
+
+        System.out.println(numero2);
+
+        var numero3 = 40;
+
+        String nombre;
+
+        nombre = "andres";
+
+        if (numero > 10){
+            nombre = "biel";
+        }
+        System.out.println("nombre = " + nombre);
+
+    }
+}
---
+++ src/Primitivos.java
@@ -0,0 +1,41 @@
+public class Primitivos {
+    public static void main(String [] args){
+
+
+
+    // entre -128 a 127
+    byte numeroByte = 127;
+    System.out.println("numeroByte = " + numeroByte);
+    System.out.println("tipo de byte corresponde en byte a" + Byte.BYTES);
+    System.out.println("tipo de byte corresponde en bits a" + Byte.SIZE);
+    System.out.println("valor maximo de un Byte = " + Byte.MAX_VALUE);
+    System.out.println("valor maximo de un Byte = " + Byte.MIN_VALUE);
+
+    // entre -32768 a 32767
+    short numeroShort = 32767;
+    System.out.println("numeroShort = " + numeroShort);
+    System.out.println("tipo de short corresponde en byte a" + Short.BYTES);
+    System.out.println("tipo de short corresponde en bits a" + Short.SIZE);
+    System.out.println("valor maximo de un short = " + Short.MAX_VALUE);
+    System.out.println("valor maximo de un short = " + Short.MIN_VALUE);
+
+    // entre -2147483648 a 214783647
+    int numeroInt = 214783647;
+    System.out.println("numeroShort = " + numeroShort);
+    System.out.println("tipo de int corresponde en byte a" + Integer.BYTES);
+    System.out.println("tipo de int corresponde en bits a" + Integer.SIZE);
+    System.out.println("valor maximo de un int = " + Integer.MAX_VALUE);
+    System.out.println("valor maximo de un int = " + Integer.MIN_VALUE);
+
+    // entre -9223372036854775808L a 9223372036854775807L
+    long numeroLong = 9223372036854775807L;
+    System.out.println("numeroShort = " + numeroShort);
+    System.out.println("tipo de int corresponde en byte a" + Integer.BYTES);
+    System.out.println("tipo de int corresponde en bits a" + Integer.SIZE);
+    System.out.println("valor maximo de un int = " + Integer.MAX_VALUE);
+    System.out.println("valor maximo de un int = " + Integer.MIN_VALUE);
+
+    }
+
+}
+
