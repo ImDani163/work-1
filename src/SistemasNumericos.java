b47cdc8f58f48552b152aae280912eb10f533b6e
ImDani163 <68122758+ImDani163@users.noreply.github.com>
2024-07-28 02:33:39 +0200

    Sistemas numericos

--- src/SistemasNumericos.java
+++ src/SistemasNumericos.java
@@ -1,2 +1,33 @@
+import javax.swing.*;
+import javax.swing.plaf.InsetsUIResource;
+
 public class SistemasNumericos {
+
+    public static void main(String[] args) {
+
+
+
+        String numeroStr = JOptionPane.showInputDialog(null, "Ingrese el numero entero");
+        int numeroDecimal = Integer.parseInt(numeroStr);
+
+        System.out.println("numeroDecimal = " + numeroDecimal);
+
+        System.out.println("numero binario de = " + numeroDecimal + " = " + Integer.toBinaryString(numeroDecimal));
+
+        int numeroBinario = 0b111110100;
+        System.out.println("numeroBinario = " + numeroDecimal);
+
+        System.out.println("numero octal de = " + numeroDecimal + " = " + Integer.toOctalString(numeroDecimal));
+        int numeroOctal = 0764;
+        System.out.println("numeroOctal = " + numeroOctal);
+
+        System.out.println("numero hexadecimal de = " + numeroDecimal + " = " + Integer.toHexString(numeroDecimal));
+        int numeroHex = 0x1f4;
+        System.out.println("numeroHex = " + numeroHex);
+
+
+    }
+
+
+
 }
